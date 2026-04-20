88a07ee86ce811086f286734d7cb85f2061fed75
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-04-20 12:17:56 +0530

    changes

---
+++ datasource/variables.tf
@@ -0,0 +1,58 @@
+
+variable "instance_names" {
+    type        =  list(string)
+    default     =  ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "ami id"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  description = "type of instance"
+}
+
+variable "tag" {
+  type        = string
+  default     = "expense"
+  description = "description"
+}
+
+variable "allow_ssh" {
+  type        = string
+  default     = "allow_ssh"
+}
+
+variable "port" {
+  type        = number
+  default     = 22
+}
+
+
+variable "protocol" {
+  type        = string
+  default     = "tcp"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "bharath.bond"
+}
+
+variable "zone_id" {
+    default = "Z07539413J8MSA1ZN61QZ"
+}
+
