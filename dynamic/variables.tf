7ae3c93f4a18aa8266e22c0aabce93dc87a53b13
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-04-21 16:08:32 +0530

    changes

---
+++ dynamic/variables.tf
@@ -0,0 +1,43 @@
+
+variable "ingress_rules" {
+  
+    default     =  [
+       {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] 
+        description = "opening 22" 
+       },
+       {
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] 
+        description = "opening 80" 
+       },
+       {
+        from_port        = 8080
+        to_port          = 8080
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] 
+        description = "opening 8080" 
+       }
+    ]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "allow_ssh" {
+  type        = string
+  default     = "allow_ssh"
+}
+
+
