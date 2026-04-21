de2ca74dea43c3e7b464cc1b0771f03b931e9141
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-04-21 16:24:50 +0530

    changes

---
+++ for-each/data.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "web" {
+
+	owners = ["973714476881"] 
+	
+	filter {
+		name   = "name"
+		values = ["Redhat-9-DevOps-Practice"]
+	}
+	
+	filter {
+		name   = "root-device-type"
+		values = ["ebs"]
+	}
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    most_recent = true
+
+}
